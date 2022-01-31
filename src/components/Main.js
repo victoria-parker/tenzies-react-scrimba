3ce629c5d6a44db019332dbde2c93a2c86220a23
victoria-parker <victoriaparkerb@gmail.com>
2022-01-31 23:25:54 +0000

    main, style, app, index added

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Main(){
+    return(
+        <div className='container'>
+            <div className='board'>
+                <p>Prueba</p>
+            </div>
+        </div>
+    )
+}
