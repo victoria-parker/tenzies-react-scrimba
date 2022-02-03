1fe5c8b28de693ca6e21df48dfe9ff421880f491
victoria-parker <victoriaparkerb@gmail.com>
2022-02-03 10:46:31 +0000

    function to create random dice numbers created and numbers passed to the die component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import Main from './components/Main';
+import Die from './components/Die';
 import './style.css';
 
+
 export default function App(){
-    return (
-        <div>
-            < Main />
-        </div>
+    const [dice, setDice]=React.useState(allNewDice())
+
+    function allNewDice(){
+        let arr=[]
+        for(let i=0; i < 10; i++){
+            let randomNumber=Math.floor(Math.random() * 6) + 1;
+            arr.push(randomNumber)
+        }
+        return arr;
+    }
+
+    
+    const diceElements=dice.map((value,index) => <Die key={index} number={value} />)
+
+    return(
+        <main>
+            <div className='container'>
+            {diceElements}
+            </div>
+        </main>
     )
 }
