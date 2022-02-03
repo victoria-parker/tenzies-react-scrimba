3fe6acd55b5f19bce52e3865c0e63b74b66736b0
victoria-parker <victoriaparkerb@gmail.com>
2022-02-03 09:44:06 +0000

    style adapted

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,7 @@ import React from 'react';
 
 export default function Main(){
     return(
-        <div className='container'>
-            <div className='board'>
-                <p>Prueba</p>
-            </div>
-        </div>
+        <main>
+        </main>
     )
 }
