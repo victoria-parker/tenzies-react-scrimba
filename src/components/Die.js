b96287da65c825677af2e3534dca372ad38575be
victoria-parker <victoriaparkerb@gmail.com>
2022-02-03 10:13:36 +0000

    die component done and styled

---
+++ src/components/Die.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Die(props){
+return (
+    <div className='die'>
+        <h2>{props.number}</h2>
+    </div>
+)
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Die from './Die';
 
 
 export default function Main(){
     return(
         <main>
+            <div className='container'>
+            < Die number='1' />
+            < Die number='2' />
+            < Die number='3' />
+            < Die number='4' />
+            < Die number='5' />
+            < Die number='6' />
+            < Die number='1' />
+            < Die number='2' />
+            < Die number='3' />
+            < Die number='4' />
+            </div>
         </main>
     )
 }
